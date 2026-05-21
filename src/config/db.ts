893fd66019f4ff80908e44c1d3bbad6f7bf38b4b
Shihab5500 <shihabahmed6501@gmail.com>
2026-05-21 08:24:01 +0600

    Update DB config for live database

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,11 +3,11 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-// ডাটাবেজ কানেকশন পুল তৈরি
-export const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  port: Number(process.env.DB_PORT),
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
+
+export default pool;
